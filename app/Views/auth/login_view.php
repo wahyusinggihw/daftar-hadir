36203a9d43e41163e970bce157e2fdc85744c877
Black07Jack <Black07Jack@users.noreply.github.com>
2023-11-16 14:59:44 +0800

    Responsive login

--- app/Views/auth/login_view.php
+++ app/Views/auth/login_view.php
@@ -7,7 +7,10 @@
     <title>Meeting Check <?php isset($title) ? print('- ' . $title) : '' ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" />
+
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/reset.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/login.css'); ?>">
+
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
