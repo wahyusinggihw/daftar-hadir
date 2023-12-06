513d340cd01688789505b95c64dd0ddbc195a0d4
Black07Jack <Black07Jack@users.noreply.github.com>
2023-12-06 15:33:30 +0800

    Membuat tampilan input code rapat baru v2

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,46 +1,73 @@
-document.addEventListener("keydown", function(event) {
-    if (event.key === "Backspace") {
-        event.preventDefault(); // Prevent browser navigation when backspace is pressed
-        clearAllInputsAndFocus();
-    }
+// document.addEventListener("keydown", function(event) {
+//     if (event.key === "Backspace") {
+//         event.preventDefault(); // Prevent browser navigation when backspace is pressed
+//         clearAllInputsAndFocus();
+//     }
+// });
+
+// function clearAllInputsAndFocus() {
+//     document.getElementById('otpInput1').value = '';
+//     document.getElementById('otpInput2').value = '';
+//     document.getElementById('otpInput3').value = '';
+//     document.getElementById('otpInput4').value = '';
+//     document.getElementById('otpInput5').value = '';
+//     document.getElementById('otpInput6').value = '';
+    
+//     // Clear the hidden input as well
+//     document.getElementById('id_rapat').value = '';
+
+//     // Set focus to otpInput1
+//     document.getElementById('otpInput1').focus();
+// }
+
+// function moveToNext(input) {
+//     const maxLength = parseInt(input.getAttribute("maxlength"));
+//     const currentLength = input.value.length;
+//     // const editor = document.getElementById('otpInput1');
+//     // editor.onpaste = pasteOTP;
+
+//     if (currentLength >= maxLength) {
+//     const nextInput = input.nextElementSibling;
+
+//     if (nextInput && nextInput.tagName === "INPUT") {
+//         nextInput.focus();
+//     } else if (nextInput && nextInput.tagName === "SPAN") {
+//         const nextInputAfterSpan = nextInput.nextElementSibling;
+//         if (nextInputAfterSpan && nextInputAfterSpan.tagName === "INPUT") {
+//         nextInputAfterSpan.focus();
+//         }
+//     }
+
+//     combineOtpValues();
+//     }
+// }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    function OTPInput() {
+        const editor = document.getElementById('otpInput1');
+        editor.onpaste = pasteOTP;
+
+        const inputs = document.querySelectorAll('#otp > *[id]');
+        for (let i = 0; i < inputs.length; i++) { 
+            inputs[i].addEventListener('input', function(event) { 
+                if(!event.target.value || event.target.value == '' ){
+                    if(event.target.previousSibling.previousSibling){
+                        event.target.previousSibling.previousSibling.focus();    
+                    }
+                
+                }else{ 
+                    if(event.target.nextSibling.nextSibling){
+                        event.target.nextSibling.nextSibling.focus();
+                    }
+                }
+                combineOtpValues();               
+            });             
+        } 
+    } 
+    OTPInput(); 
 });
 
-function clearAllInputsAndFocus() {
-    document.getElementById('otpInput1').value = '';
-    document.getElementById('otpInput2').value = '';
-    document.getElementById('otpInput3').value = '';
-    document.getElementById('otpInput4').value = '';
-    document.getElementById('otpInput5').value = '';
-    document.getElementById('otpInput6').value = '';
-    
-    // Clear the hidden input as well
-    document.getElementById('id_rapat').value = '';
-
-    // Set focus to otpInput1
-    document.getElementById('otpInput1').focus();
-}
-
-function moveToNext(input) {
-    const maxLength = parseInt(input.getAttribute("maxlength"));
-    const currentLength = input.value.length;
-    // const editor = document.getElementById('otpInput1');
-    // editor.onpaste = pasteOTP;
-
-    if (currentLength >= maxLength) {
-    const nextInput = input.nextElementSibling;
-
-    if (nextInput && nextInput.tagName === "INPUT") {
-        nextInput.focus();
-    } else if (nextInput && nextInput.tagName === "SPAN") {
-        const nextInputAfterSpan = nextInput.nextElementSibling;
-        if (nextInputAfterSpan && nextInputAfterSpan.tagName === "INPUT") {
-        nextInputAfterSpan.focus();
-        }
-    }
-
-    combineOtpValues();
-    }
-}
 function pasteOTP(event){
     event.preventDefault();
     let elm = event.target;
@@ -70,3 +97,10 @@ function combineOtpValues() {
 
     document.getElementById('id_rapat').value = combinedOtp;
 }
+var otpInputs = document.querySelectorAll('.otp-input');
+        otpInputs.forEach(function(input) {
+            input.addEventListener('input', combineOtpValues);
+            input.addEventListener('paste', function() {
+                setTimeout(combineOtpValues, 0);
+            });
+        });
