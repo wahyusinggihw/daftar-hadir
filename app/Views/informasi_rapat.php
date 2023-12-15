03c880e73d1c2d40e99fdc8712f4cc2f70d55020
Black07Jack <Black07Jack@users.noreply.github.com>
2023-12-15 12:26:37 +0800

    Re-Design Halaman Berhasil dan Gagal (Status ASN Belom)

--- app/Views/informasi_rapat.php
+++ app/Views/informasi_rapat.php
@@ -20,60 +20,6 @@
     <?php endif; ?>
 
     <div class="card-info">
-        <div class="content-timeline">
-            <ul class="timeline">
-                <li>
-                    <div class="item">
-                        <span>1</span>
-                        <div class="content">
-                            <h3>Masukkan Kode Rapat</h3>
-                            <p>Silahkan masukkan kode rapat yang sudah diberikan di kolom diatas, lalu tekan "Masuk". Atau</p>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="item">
-                        <span>2</span>
-                        <div class="content">
-                            <h3>Masuk Dengan Qr Code</h3>
-                            <p>Cara lain untuk memasuki rapat adalah dengan memindai kode QR yang sudah diberikan oleh admin rapat.</p>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="item">
-                        <span>3</span>
-                        <div class="content">
-                            <h3>Memilih Status</h3>
-                            <p>Jika sudah berhasil maka silahkan anda memilih sebagai tamu atau pegawai. Jika sebagai pegawai silahkan di pilih apakah anda "ASN" atau "non-ASN", Namun jika sebagai tamu silahkan di pilih "tamu".</p>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="item">
-                        <span>4</span>
-                        <div class="content">
-                            <h3>Mengisi Formulir</h3>
-                            <p>
-                                Jika sebagai pegawai cukup isikan NIP atau NIPT, lalu tekan "Cari" dan tanda tangan. Sedangkan untuk tamu jika baru pertama anda bisa isi semua biodata anda pada formulir yang sudah di sediakan, jika NIK kalian sudah terdaftar
-                                silahkan tekan "Cari" lalu lanjutkan mengisi tanda tangan.
-                            </p>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="item">
-                        <span>5</span>
-                        <div class="content">
-                            <h3>Selesai</h3>
-                            <p>
-                                Pastikan semua data yang anda masukkan sudah benar, kemudian anda dapat mengirim formulir dengan menekan tombol "Kirim".
-                            </p>
-                        </div>
-                    </div>
-                </li>
-            </ul>
-        </div>
         <div class="content-wrapper">
             <div class="card-text">
                 <h1><?= $agendaRapat['agenda_rapat'] ?></h1>
