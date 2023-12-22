0122430c2de113e754562c1c0dbc80b27f9a1847
Black07Jack <Black07Jack@users.noreply.github.com>
2023-12-23 00:15:00 +0800

    Menata Css menjadi satu di bagian admin clear

--- app/Views/admin/templates/header.php
+++ app/Views/admin/templates/header.php
@@ -6,10 +6,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Dashboard <?php isset($title) ? print('- ' . $title) : '' ?></title>
   <link rel="shortcut icon" href="<?= base_url('assets/img/icon.png') ?>" type="image/x-icon">
-  <link rel="stylesheet" href="<?= base_url('assets/css/agendarapat.css') ?>">
-  <link rel="stylesheet" href="<?= base_url('assets/css/daftarpeserta.css') ?>">
-  <link rel="stylesheet" href="<?= base_url('assets/css/agendakosong.css') ?>">
-  <link rel="stylesheet" href="<?= base_url('assets/css/admin.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/back_end/style_admin.css') ?>">
+  <!-- <link rel="stylesheet" href="<?= base_url('assets/css/agendarapat.css') ?>"> -->
+  <!-- <link rel="stylesheet" href="<?= base_url('assets/css/daftarpeserta.css') ?>"> -->
+  <!-- <link rel="stylesheet" href="<?= base_url('assets/css/agendakosong.css') ?>"> -->
+  <!-- <link rel="stylesheet" href="<?= base_url('assets/css/admin.css') ?>"> -->
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
