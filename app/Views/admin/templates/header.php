e0e0030b328f0ce1a3ed35e5867a104cf90775a5
Black07Jack <Black07Jack@users.noreply.github.com>
2023-12-27 01:52:48 +0800

    Pemindahan code CSS dan JS Admin LTE

--- app/Views/admin/templates/header.php
+++ app/Views/admin/templates/header.php
@@ -12,19 +12,19 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?= base_url('assets/plugins/fontawesome-free/css/all.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/back_end/admin_lte/plugins/fontawesome-free/css/all.min.css') ?>">
   <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="<?= base_url('assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/back_end/admin_lte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') ?>">
   <!-- iCheck -->
-  <link rel="stylesheet" href="<?= base_url('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/back_end/admin_lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') ?>">
   <!-- JQVMap -->
-  <link rel="stylesheet" href="<?= base_url('assets/plugins/jqvmap/jqvmap.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/back_end/admin_lte/plugins/jqvmap/jqvmap.min.css') ?>">
   <!-- Theme style -->
-  <link rel="stylesheet" href="<?= base_url('assets/dist/css/adminlte.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/back_end/admin_lte/dist/css/adminlte.min.css') ?>">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="<?= base_url('assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/back_end/admin_lte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') ?>">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="<?= base_url('assets/plugins/daterangepicker/daterangepicker.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/back_end/admin_lte/plugins/daterangepicker/daterangepicker.css') ?>">
   <!-- summernote -->
   <link rel="stylesheet" href="<?= base_url('assets/plugins/summernote/summernote-bs4.min.css') ?>">
 
